58ccc49e3b4fb8f7cb093f8abca96bbe1d2bf8af
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-09-30 12:57:00 +0530

    1.11

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 "use client"
 
 import React, { useEffect } from 'react'
 import Navbar from '@/app/(components)/Navbar'
 import Sidebar from '@/app/(components)/Sidebar'
 import StoreProvider, { useAppSelector } from './redux'
-=======
-import React from 'react';
-import Navbar from '@/app/(components)/Navbar';
-import Sidebar from '@/app/(components)/Sidebar';
->>>>>>> 73fe80c03703fb4670ec99cc620e0125b5dae6bf
 
 const DashboardLayout = ({children}: {children: React.ReactNode} ) => {
   const isSidebarCollapsed = useAppSelector((state)=> state.global.isSidebarCollapsed,);
@@ -25,11 +19,7 @@ const DashboardLayout = ({children}: {children: React.ReactNode} ) => {
     <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
         {/*sidebar*/}
        <Sidebar />
-<<<<<<< HEAD
         <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg ${ isSidebarCollapsed ? "": "md:pl-64"}`}>
-=======
-        <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
->>>>>>> 73fe80c03703fb4670ec99cc620e0125b5dae6bf
           <Navbar />
           {children}
         </main>
