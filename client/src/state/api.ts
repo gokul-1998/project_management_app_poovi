2c24fbef9be289cd8fbdd036c1d7712d4ead7c60
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-10-01 18:20:11 +0530

    3.05.22

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -82,8 +82,23 @@ export const api = createApi({
             query: ({ projectId }) => `task?projectId=${projectId}`,
             providesTags: (result) => result? result.map(({ id }) => ({ type: "Tasks" as const, id })): [{ type: "Tasks" as const}],
         }),
-
+        createTask: build.mutation<Task, Partial<Task>>({
+            query: (task) => ({
+                url: "tasks",
+                method: "POST",
+                body: task,
+            }),
+            invalidatesTags: ["Tasks"],
+        }),
+        updateTaskStatus: build.mutation<Task, { taskId: number, status: string}>({
+            query: ({taskId, status}) => ({
+                url: "tasks/${taskId}/status",
+                method: "PATCH",
+                body: { status},
+            }),
+            invalidatesTags: (result, error, { taskId }) => [{ type: "Tasks", id: taskId }],
+        }),
     }),
 })
 
-export const { useGetProjectsQuery, useCreateProjectMutation} =api;
+export const { useGetProjectsQuery, useCreateProjectMutation, useGetTasksQuery, useCreateTaskMutation } =api;
