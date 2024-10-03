74fe5efec3a76fc49207450706b9b4e7c0feab1c
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-10-03 17:44:51 +0530

    5.07.09

---
+++ client/src/app/projects/ModalNewProject/index.tsx
@@ -0,0 +1,29 @@
+import { useCreateProjectMutation } from '@/state/api';
+import React, { useState } from 'react'
+
+type Props = {
+    isOpen: boolean;
+    onClose: () => void;
+}
+
+const ModalNewProject = ({ isOpen, onClose }: Props) => {
+  const [createProject,{isLoading}] = useCreateProjectMutation()
+  const [projectName, setProjectName] = useState('');
+  const [description, setDescription] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [teamId, setTeamId] = useState('');
+  
+  const handleSubmit = async ()=> {
+    if (!projectName || !startDate || !endDate ) return;
+
+    await
+  
+  return 
+    <div>
+      Modal
+    </div>
+  
+}
+
+export default ModalNewProject
