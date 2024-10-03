f24536cdd1b35de7324acfc8b3ee8118c744d5e6
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-10-03 18:35:33 +0530

    5.15.05

--- client/src/app/projects/ModalNewProject/index.tsx
+++ client/src/app/projects/ModalNewProject/index.tsx
@@ -33,7 +33,50 @@ const ModalNewProject = ({ isOpen, onClose }: Props) => {
   return (
     <Modal
        isOpen={isOpen} onClose={onClose} name=" Create New Project">
+      <form
+        className="mt-4 space-y-6"
+        onSubmit={(e)=>{
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <input 
+            type="text" 
+            className={inputStyles} 
+            placeholder="Project Name" 
+            value={projectName} 
+            onChange={(e)=> setProjectName(e.target.value)}
+        />
+        <textarea 
+            className={inputStyles} 
+            placeholder="Description" 
+            value={description} 
+            onChange={(e)=> setDescription(e.target.value)}
+        />
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 sm:gap-2">
+            <input 
+                type="date" 
+                className={inputStyles} 
+                value={startDate} 
+                onChange={(e)=> setStartDate(e.target.value)}
+            />
+            <input 
+                type="date" 
+                className={inputStyles} 
+                value={endDate} 
+                onChange={(e)=> setEndDate(e.target.value)}
+            />
+        </div>
+        <button
+          type="submit"
+          className={`mt-4 flex w-full justify-center rounded-md border border-transparent bg-blue-primary px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 focus-offset-2 ${isFormValid() || isLoading ? "cursor-not-allowed opacity-50" : ""}`}
+          disabled={!isFormValid() || isLoading}
+        >
+          {isLoading ? "Creating..." : "Create Project"}
+        </button>
+        
 
+      </form>
     </Modal>
   )
 }
