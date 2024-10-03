7fb839c7da4bae7ab39cf5c70d69caa6e3fe22ea
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-10-03 17:57:35 +0530

    5.09.19

--- client/src/app/projects/ModalNewProject/index.tsx
+++ client/src/app/projects/ModalNewProject/index.tsx
@@ -1,3 +1,4 @@
+import Modal from '@/components/Modal';
 import { useCreateProjectMutation } from '@/state/api';
 import React, { useState } from 'react'
 
@@ -12,18 +13,29 @@ const ModalNewProject = ({ isOpen, onClose }: Props) => {
   const [description, setDescription] = useState('');
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
-  const [teamId, setTeamId] = useState('');
   
   const handleSubmit = async ()=> {
     if (!projectName || !startDate || !endDate ) return;
 
-    await
+    await createProject({
+      name: projectName,
+      description,
+      startDate,
+      endDate,
+    })
+  }
+
+  const isFormValid = () => {
+    return projectName && description && startDate && endDate;
+  }
   
+  const inputStyles="w-full rounded border border-gray-300 p-2 shadow-sm dark:border-dark-tertiary dark:bg-dark-tertiary dark:text-white dark:focus:outline-none"
   return 
-    <div>
-      Modal
-    </div>
-  
+    <Modal
+       isOpen={isOpen} onClose={onClose} name=" Create New Project">
+
+    </Modal>
+   
 }
 
 export default ModalNewProject
