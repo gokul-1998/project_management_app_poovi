2db6499f5915fc184d994bca4bb806ac0816489e
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-10-04 12:24:59 +0530

    5.43.37

--- client/src/app/timeline/page.tsx
+++ client/src/app/timeline/page.tsx
@@ -2,7 +2,7 @@
 
 import { useAppSelector } from '@/app/redux';
 import Header from '@/components/Header';
-import { useGetProjectsQuery, useGetTasksQuery } from '@/state/api';
+import { useGetProjectsQuery } from '@/state/api';
 import { DisplayOption, Gantt, ViewMode } from 'gantt-task-react';
 import 'gantt-task-react/dist/index.css';
 import React, { useMemo, useState } from 'react'
