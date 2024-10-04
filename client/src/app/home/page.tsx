c8518d2ab4cec961ce79cebd81c418d5451bb836
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-10-04 18:28:57 +0530

    6.36.53

---
+++ client/src/app/home/page.tsx
@@ -0,0 +1,64 @@
+"use client"
+
+import { Priority, Project, Task, useGetProjectsQuery, useGetTasksQuery } from '@/state/api';
+import React from 'react'
+import { useAppSelector } from '../redux';
+import { GridColDef } from '@mui/x-data-grid';
+
+
+const HomePage =() => {
+    const { 
+        data: tasks, 
+        isLoading: tasksLoading, 
+        isError: tasksError,
+    } = useGetTasksQuery({projectId: parseInt("1")}); 
+    const { data:projects, isLoading: isProjectsLoading } = useGetProjectsQuery();
+
+    const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+    if (tasksLoading || isProjectsLoading) return <div>Loading...</div>;
+    if (tasksError || !tasks || !projects) return <div>Error fetching data</div>;
+
+    const priorityCount = tasks.reduce(
+        (acc: Record<string, number>, task:Task) => {
+            const { priority } = task;
+            acc[priority as Priority] = (acc[priority as Priority] || 0) + 1;
+            return acc;
+        },
+        {},
+    )
+
+    const taskDistribution = Object.keys(priorityCount).map((key) => ({
+         name: key,
+         count: priorityCount[key],
+    }))
+    const statusCount = projects.reduce(
+        (acc: Record<string, number>, project:Project) => {
+            const status = project.endDate ? "Completed" : "Active";
+            acc[status] = (acc[status] || 0) + 1;
+            return acc;
+        },
+        {},
+    )
+
+    const projectStatus = Object.keys(statusCount).map((key) => ({
+        name: key,
+        count: statusCount[key],
+   }))
+
+   const taskColumns: GridColDef[] = [
+    { field: "title", headerName: "Title", width: 200 },
+    { field: "status", headerName: "Status", width: 150 },
+    { field: "priority", headerName: "Priority", width: 150 },
+    { field: "dueDate", headerName: "Due Date", width: 150 },
+   ]
+
+
+
+
+  return (
+    <div>HomePage</div>
+  )
+}
+
+export default HomePage
