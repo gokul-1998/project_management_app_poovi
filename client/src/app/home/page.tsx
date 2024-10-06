2ee2f245fa7d8af16aa75d49f6d8efd473cf35db
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-10-06 18:18:01 +0530

    6.42.25

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -79,7 +79,7 @@ const HomePage =() => {
                 <ResponsiveContainer width="100%" height={300}>
                     <BarChart data={taskDistribution}>
                         <CartesianGrid strokeDasharray="3 3" stroke={chartColors.barGrid} />
-                        <XAxis dataKey="name"/>
+                        <XAxis dataKey="name" />
                     </BarChart>
 
                 </ResponsiveContainer>
