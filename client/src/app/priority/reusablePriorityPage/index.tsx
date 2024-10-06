fa3e05ba9ebd38236c95dbd2d404c630af7c9a80
Poovendranselvaraj <poovendranselvaraj333@gmail.com>
2024-10-06 19:58:15 +0530

    7..3.00

--- client/src/app/priority/reusablePriorityPage/index.tsx
+++ client/src/app/priority/reusablePriorityPage/index.tsx
@@ -1,19 +1,21 @@
 "use client"
 
 import { useAppSelector } from '@/app/redux'
+import ModalNewTask from '@/components/ModalNewTask'
 import { Priority, Task, useGetTasksByUserQuery } from '@/state/api'
+import { Modal } from '@mui/material'
 import React, { useState } from 'react'
 
 type Props = {
     priority:Priority
 }
 
-const index = ({priority}: Props) => {
+const ReusablePriorityPage = ({priority}: Props) => {
     const [view, setView] = useState("list");
     const [ isModalNewTaskOpen, setIsModalNewTaskOpen ] = useState(false);
     
     const userId = 1;
-    const { data:task, isLoading, isError: isTasksError } = useGetTasksByUserQuery( userId || 0, {
+    const { data:tasks, isLoading, isError: isTasksError } = useGetTasksByUserQuery( userId || 0, {
         skip: userId === null
     })
 
@@ -21,9 +23,12 @@ const index = ({priority}: Props) => {
 
     const filteredTasks = tasks?.filter((task:Task)=> task.priority === priority);
 
+    if (isTasksError || !tasks) return <div>Error fetching tasks</div>;
   return (
-    <div>index</div>
+    <div className="m-5 p-4">
+        <ModalNewTask isOpen=  {isModalNewTaskOpen} onClose={() => setIsModalNewTaskOpen(false)} />
+    </div>
   )
 }
 
-export default index
+export default ReusablePriorityPage;
