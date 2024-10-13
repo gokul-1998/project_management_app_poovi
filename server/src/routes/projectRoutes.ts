7b0f26a91a877cf804032ba369f3dcbed4b99a5e
Gokulakrishnan M <82328858+gokul-1998@users.noreply.github.com>
2024-10-13 10:01:01 +0000

    Added more

--- server/src/routes/projectRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { createProjects, getProjects } from "../controllers/projectController";
-
-const router = Router();
-
-router.get("/", getProjects);
-router.post("/", createProjects);
-
-export default router;
